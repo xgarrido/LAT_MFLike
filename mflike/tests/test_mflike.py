c05d52cddce0b65854429c4c038ff6fb4f1b4fc1
Xavier Garrido <xavier.garrido@gmail.com>
2019-12-04 23:13:39 +0100

    relax equality checks

--- mflike/tests/test_mflike.py
+++ mflike/tests/test_mflike.py
@@ -47,14 +47,11 @@ class MFLikeTest(unittest.TestCase):
         powers = results.get_cmb_power_spectra(pars, CMB_unit="muK")
         cl_dict = {k: powers["total"][:, v]
                    for k, v in {"tt": 0, "ee": 1, "te": 3}.items()}
-        loglike = my_mflike.loglike(cl_dict, **nuisance_params)
-        self.assertAlmostEqual(-2 * loglike, chi2s["tt-te-ee"], 7)
-
         for select, chi2 in chi2s.items():
             my_mflike = MFLike({"path_install": modules_path,
                                 "sim_id": 0, "select": select})
             loglike = my_mflike.loglike(cl_dict, **nuisance_params)
-            self.assertAlmostEqual(-2 * loglike, chi2, 7)
+            self.assertAlmostEqual(-2 * loglike, chi2, 3)
 
     def test_cobaya(self):
         info = {"likelihood": {"mflike.MFLike": {"sim_id": 0}},
@@ -64,4 +61,4 @@ class MFLikeTest(unittest.TestCase):
         from cobaya.model import get_model
         model = get_model(info)
         chi2 = -2 * model.loglikes(nuisance_params)[0]
-        self.assertAlmostEqual(chi2[0], chi2s["tt-te-ee"], 7)
+        self.assertAlmostEqual(chi2[0], chi2s["tt-te-ee"], 3)
